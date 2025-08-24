5a27492d444426f08fe69fc2c7791e49a972ca77
charles-m-doan <cmatthewdoan@gmail.com>
2025-08-24 13:35:23 -0400

    Sun, Aug 24, 2025  1:35:23 PM

--- src/app/features/leaderboard/leaderboard.component.ts
+++ src/app/features/leaderboard/leaderboard.component.ts
@@ -12,36 +12,12 @@ import { CommonModule } from '@angular/common';
 })
 export class LeaderboardComponent implements OnInit {
 
-  @ViewChild('bgm', { static: true }) bgm!: ElementRef<HTMLAudioElement>;
-
-  public readonly text$: Observable<string>;
-  public readonly doontXlsx$: Observable<Record<string, any[]>>;
-  public readonly repoFiles$: Observable<string[]>;
-
   constructor(private apiService: ApiService) {
-    this.text$ = this.apiService.testResponse$;
-    this.doontXlsx$ = this.apiService.doontXlsx$;
-    this.repoFiles$ = this.apiService.repoFiles$;
+
   }
 
   ngOnInit(): void {
-    this.apiService.getTestFile();
-    this.apiService.getDoontXlsx();
-    this.apiService.getRepoFiles();
-  }
 
-  ngAfterViewInit() {
-    const el = this.bgm.nativeElement;
-    el.play().catch(() => { });
-    const unlock = () => {
-      el.muted = false;
-      el.play().catch(() => { });
-      window.removeEventListener('pointerdown', unlock);
-      window.removeEventListener('keydown', unlock);
-      console.log("UNMUTE!");
-    };
-    window.addEventListener('pointerdown', unlock, { once: true });
-    window.addEventListener('keydown', unlock, { once: true });
   }
 
 }
